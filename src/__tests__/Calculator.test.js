5a13f2ce05219f911ae6db387fba54e47aa8cab8
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-12-07 13:34:03 +0100

    Add tests for Calculator and CalculatorButton

---
+++ src/__tests__/Calculator.test.js
@@ -0,0 +1,10 @@
+import renderer from 'react-test-renderer';
+import Calculator from '../components/Calculator';
+import buttonsInfo from '../buttons';
+
+it('renders Calculator correctly', () => {
+  const tree = renderer
+    .create(<Calculator buttons={buttonsInfo} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
