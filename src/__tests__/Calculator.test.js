ef86369eeec21fd93b585cd658a19a4696e6c6de
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-12-08 08:47:33 +0100

    Add test for Nabvar

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -3,8 +3,6 @@ import Calculator from '../components/Calculator';
 import buttonsInfo from '../buttons';
 
 it('renders Calculator correctly', () => {
-  const tree = renderer
-    .create(<Calculator buttons={buttonsInfo} />)
-    .toJSON();
+  const tree = renderer.create(<Calculator buttons={buttonsInfo} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/__tests__/CalculatorButtun.test.js
+++ src/__tests__/CalculatorButtun.test.js
@@ -2,8 +2,6 @@ import renderer from 'react-test-renderer';
 import CalculatorButtun from '../components/CalculatorButton';
 
 it('renders CalculatorButtun correctly', () => {
-  const tree = renderer
-    .create(<CalculatorButtun text="1" changeBg={false} handleClick={() => {}} />)
-    .toJSON();
+  const tree = renderer.create(<CalculatorButtun text="1" changeBg={false} handleClick={() => {}} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
